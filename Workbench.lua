e124c321d10bdabef1ba075a98ceaae2d76abb75
Lorenz Saalmann <l.s.spirit@icloud.com>
2025-01-08 01:43:03 +0100

    created a lua script to simulate the time dependent potential of the pulse plate

--- Workbench.lua
+++ Workbench.lua
@@ -1,4 +1,25 @@
 simion.workbench_program()
+simion.print("Running custom Workbench.lua")
+local pe_update_period_usec = 0.010  -- microseconds
+local last_pe_update = 0
+local max_voltage = 3500 -- volts
+local rise_time = .1 -- microseconds
+local fall_time = .1 -- microseconds
+local pulse_duration = 4.5 -- microseconds
 
-function segment.other_actions()
+local current_voltage = 0
+
+function segment.fast_adjust()
+    -- Calculate the current voltage based on the time of flight (rise -> stay at max for pulse_duration -> fall)
+    if ion_time_of_flight < rise_time then
+        current_voltage = max_voltage * ion_time_of_flight / rise_time
+    elseif ion_time_of_flight < rise_time + pulse_duration then
+        current_voltage = max_voltage
+    elseif ion_time_of_flight < rise_time + pulse_duration + fall_time then
+        current_voltage = max_voltage * (rise_time + pulse_duration + fall_time - ion_time_of_flight) / fall_time
+    else
+        current_voltage = 0
+    end
+
+    adj_elect01 = current_voltage
 end
