4d3d2da4f70b9efb085b699244115101bfa3ba1f
Lorenz Saalmann <l.s.spirit@icloud.com>
2025-01-29 15:11:34 +0100

    added Pos_Forward_Bias

--- Analysis/Plots/position_plot.py
+++ Analysis/Plots/position_plot.py
@@ -2,4 +2,4 @@ from Position_Plots import cod_position_plotter
 
 cod_position_plotter.plot_beam_from_ascii_file('../Output/pos_210.cod', False, True, 15, show_reference=False)
 cod_position_plotter.plot_beam_from_ascii_file('../Output/positions.cod', False, True, 15, 2)
-cod_position_plotter.plot_beam_from_ascii_file('../Output/pos_forward.cod', False, True, 15, 2)
+cod_position_plotter.plot_beam_from_ascii_file('../Output/pos_forward.cod', False, True, 15, 2, False)
